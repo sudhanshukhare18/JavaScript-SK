f1b3083e01cb94fc2f342f15ff973a3c67faaf98
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-02 19:11:47 +0530

    suudd

---
+++ Jan2.js
@@ -0,0 +1,8 @@
+let ctar=document.getElementsByClassName("content")
+ctar[0].innerHTML="<i>yes</i>"
+ctar[1].textContent="Happy New Year"
+let para = document.getElementsByTagName("p")
+para[3].textContent="this is para"
+para[1].innerHTML="<b>Kal 2025 ka pehla din tha </b>"
+//what id hoisting
+//ES6 and ECMA
