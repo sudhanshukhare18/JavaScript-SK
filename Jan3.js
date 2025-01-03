bf86246ed52a9fdeafd4da2411b78dcf4b37cbb0
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-03 20:20:57 +0530

    suudd

---
+++ Jan3.js
@@ -0,0 +1,6 @@
+let head1=document.getElementById('headcontent')
+head1.textContent = "Cybrom1"
+head1.innerHTML = "<i>TECHNICAL INSTITUTE</I>"
+head1.style.color = "red"
+head1.style.backgroundColor = "yellow"
+head1.style.padding = "20px"
---
+++ Jan3_1.js
@@ -0,0 +1,11 @@
+let head1=document.getElementById('headcontent')
+head1.textContent = "Cybrom1"
+head1.innerHTML = "<i>Ajay Sir</I>"
+head1.style.color = "red"
+head1.style.backgroundColor = "yellow"
+head1.style.padding = "20px"
+
+
+
+let myimg=document.getElementById('ajaysir')
+myimg.src= "abc.png"
