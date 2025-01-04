ae5fa8fd65f688ea07b7fbc76b407dc6b15c8304
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-04 19:04:55 +0530

    suudd

--- Jan4.js
+++ Jan4.js
@@ -2,3 +2,8 @@ let head =document.createElement('h1')
 head.textContent="Hi we are Devlopers.....!!!!"
 let bd = document.body
 bd.append(head)
+bd.setAttribute('bgcolor','skyblue')
+head.setAttribute('align','center')
+let a=document.getElementById('head2')
+a.removeAttribute('align')
+a.setAttribute('style',"background-color:black;color:white ; text-align:center")
