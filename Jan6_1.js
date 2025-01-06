aa055539229ba0ad555d9683db53993545308dd2
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-06 20:02:52 +0530

    suudd

---
+++ Jan6_1.js
@@ -0,0 +1,11 @@
+function fun1() {
+    
+    let a=document.getElementById("ab")
+    a.src="Images/Screenshot 2024-12-04 225835.png"
+    
+}
+function fun2() {
+    
+    let b=document.getElementById("ab");
+    b.src="Images/Q.png"
+}
---
+++ Jan6_2.js
@@ -0,0 +1,30 @@
+function fun1() {
+    
+    let a=document.getElementById("ab")
+    a.src="Images/Screenshot 2024-12-04 225835.png"
+    
+}
+function fun2() {
+    
+    let b=document.getElementById("ab");
+    b.src="Images/Q.png"
+}
+function red(){
+    document.body.style.backgroundColor="rgb(46,10,47)"
+}
+function yellow(){
+    document.body.style.backgroundColor="rgb(34,193,195)"
+}
+function blue(){
+    document.body.style.backgroundColor="rgb(175,39,98)"
+}
+function pink(){
+    document.body.style.backgroundColor="pink"
+}
+function pink1(colorr){
+    document.body.style.backgroundColor=colorr
+}
+function f1(){
+    let para=document.getElementById('para')
+    para.classList.add('drk')
+}
