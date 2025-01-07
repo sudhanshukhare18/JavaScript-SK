8034ec96e3b125b98630a494adc0dfe76024f530
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-07 22:04:09 +0530

    suudd

---
+++ Jan7.js
@@ -0,0 +1,13 @@
+function add(){
+    let head =document.getElementById('head')
+    head.classList.add('dark')
+}
+function remove(){
+    let head =document.getElementById('head')
+    head.classList.remove('dark')
+}
+function both(){
+    let head =document.getElementById('head')
+   document.body.classList.toggle('dark')
+    head.classList.toggle('dark')
+}
---
+++ Jan7_1.js
@@ -0,0 +1,22 @@
+
+function c1(){
+    let a=180
+    let b=parseInt(prompt("How Many Chairs Want"))
+    return b;
+}
+function c2(){
+    let a=6000
+    let c=parseInt(prompt("how many chair want"))
+    return c*a;
+}
+
+if(c<0){
+
+    let h1=document.getElementById('1h');
+    h1  .textContent="Yow have Bought"
+
+}
+if(b<0){
+    document.write()
+}
+
